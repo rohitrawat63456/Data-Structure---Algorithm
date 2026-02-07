ecbcd287a40007875fc99f5a430f8f16479d7575
Rohit Rawat <122621142+rohitrawat63456@users.noreply.github.com>
2026-02-07 15:52:23 +0530

    Time: 322 ms (5.26%) | Memory: 97.7 MB (5.59%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -1,7 +1,6 @@
 class Solution {
     public int rec(String s, int prev, int[][] dp, int idx) {
         if (idx == s.length()) {
-            //System.out.println(prev);
             return 0;
         }
         if (dp[idx][prev] != -1) {
@@ -15,11 +14,11 @@ class Solution {
             take = rec(s, ch == 'a' ? 1 : 2, dp, idx + 1);
         } else {
             char p = prev == 1 ? 'a' : 'b';
-            if (p == ch || (ch == 'b' && p == 'a')) {
+            if (p == ch || (ch == 'b' || p == 'a')) {
                 take = rec(s, ch == 'a' ? 1 : 2, dp, idx + 1);
             }
         }
-        notTake = rec(s, prev, dp, idx + 1) + 1;//not take
+        notTake = rec(s, prev, dp, idx + 1) + 1;
         return dp[idx][prev] = Math.min(take, notTake);
 
     }
@@ -30,7 +29,6 @@ class Solution {
         for (int[] arr : dp) {
             Arrays.fill(arr, -1);
         }
-        // Arrays.fill(dp, -1);
         return rec(s, 0, dp, 0);
     }
 }
