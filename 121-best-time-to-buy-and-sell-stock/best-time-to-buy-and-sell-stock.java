ea2dfcb895929724dcd9f533c7c07b29600ce09e
Rohit Rawat <122621142+rohitrawat63456@users.noreply.github.com>
2026-02-16 22:21:13 +0530

    Time: 19 ms (1.60%) | Memory: 86.7 MB (99.19%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,15 +1,20 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int maxProfit = 0;
-        int purchasedStockPrice = prices[0];
-        for (int i = 1; i < prices.length; i++) {
-            int currStockPrice = prices[i];
-            if ((currStockPrice - purchasedStockPrice) > maxProfit) {
-                maxProfit = currStockPrice - purchasedStockPrice;
-            } else if (currStockPrice < purchasedStockPrice) {
-                purchasedStockPrice = currStockPrice;
+        Stack<Integer> st = new Stack<>();
+        int max = 0;
+        for (int i = prices.length - 1; i >= 0; i--) {
+            int currPrice = prices[i];
+            if (st.isEmpty()) {
+                st.push(currPrice);
+            } else {
+                int peek = st.peek();
+                if (peek > currPrice) {
+                    max = Math.max(peek - currPrice, max);
+                } else {
+                    st.push(currPrice);
+                }
             }
         }
-        return maxProfit;
+        return max;
     }
 }
