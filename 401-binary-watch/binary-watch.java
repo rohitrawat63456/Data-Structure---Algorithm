3a2b49be0b6e06266bd67dbfa6873b57fbb93f56
Rohit Rawat <122621142+rohitrawat63456@users.noreply.github.com>
2026-02-17 12:18:31 +0530

    Time: 5 ms (83.63%) | Memory: 49.6 MB (13.87%) - LeetSync

--- 401-binary-watch/binary-watch.java
+++ 401-binary-watch/binary-watch.java
@@ -1,41 +1,31 @@
 class Solution {
     List<String> l;
 
-    public void rec(int n,int h, int m, boolean[] hb, boolean[] mb) {
+    public void rec(int n, int h, int m, int idx) {
         if (h > 11 || m > 59) {
             return;//negative base case
         }
         if (n == 0) {
             String time = h + ":" + (m <= 9 ? "0" + m : m);
-            if(!l.contains(time))
             l.add(time);
             return;
         }
-        int[] hr = { 1, 2, 4, 8 };
-        int[] min = { 1, 2, 4, 8, 16,32 };
-        for (int i = 0; i < 6; i++) {
-            if (!mb[i]) {
-                mb[i] = true;
-                rec(n - 1,h, m + min[i], hb, mb);
-                mb[i] = false;
+        int[] t = { 1, 2, 4, 8, 1, 2, 4, 8, 16, 32 };
+        for (int i = idx; i < 10; i++) {
+            if (i < 4) {
+                rec(n - 1, h + t[i], m, i + 1);
+            } else {
+                rec(n - 1, h, m + t[i], i + 1);
             }
-        }
-        for (int i = 0; i < 4; i++) {
-            if (!hb[i]) {
-                hb[i] = true;
-                rec(n - 1,h + hr[i], m, hb, mb);
-                hb[i] = false;
-            }
+
         }
     }
 
     public List<String> readBinaryWatch(int turnedOn) {
         l = new ArrayList<>();
-      //  int[] hr = { 1, 2, 4, 8 };
-        boolean[] hb = new boolean[4];
-        //int[] min = { 1, 2, 4, 8, 16,32 };
-        boolean[] mb = new boolean[6];
-        rec(turnedOn, 0, 0, hb, mb);
+        // boolean[] hb = new boolean[4];
+        // boolean[] mb = new boolean[6];
+        rec(turnedOn, 0, 0,0);
         return l;
     }
 }
